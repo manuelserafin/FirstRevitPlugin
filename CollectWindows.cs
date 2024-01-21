a5efc4d1cae78611c08088234fb1e52029033000
= <=>
2024-01-20 23:44:29 -0300

    Create a collector

---
+++ CollectWindows.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using System.Dynamic;
+
+namespace MyRevitCommands
+{
+    [TransactionAttribute(TransactionMode.ReadOnly)]
+    public class CollectWindows : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get UIDocument
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            //Get Document
+            Document doc = uidoc.Document;
+
+            //Create Filtered Element Collector
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+
+            //CreateInstanceBinder Filter
+            ElementCategoryFilter filter = new ElementCategoryFilter(BuiltInCategory.OST_Windows);
+
+            //Apply Filter
+            IList<Element> windows = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements();
+
+            TaskDialog.Show("Windows", string.Format("{0} windows counted!", windows.Count));
+
+            return Result.Succeeded;
+        }
+    }
+}
--- GetElementId.cs
+++ GetElementId.cs
@@ -16,14 +16,32 @@ namespace MyRevitCommands
         {
             //Get UIDocument
             UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            //Get Document
+            Document doc = uidoc.Document;
+
             try
             {
                 //Pick Object
                 Reference pickedObj = uidoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element);
+
+                //Retrieve Element
+                ElementId eleId = pickedObj.ElementId;
+                Element ele = doc.GetElement(eleId);
+
+                //Get Element Type
+                ElementId eTypeId = ele.GetTypeId();
+                ElementType eType = doc.GetElement(eTypeId) as ElementType;
+
                 //Display Element Id
                 if (pickedObj != null)
                 {
-                    TaskDialog.Show("Element Id", pickedObj.ElementId.ToString());
+                    TaskDialog.Show("Element Classification", Environment.NewLine
+                        + "ID: " + eleId.ToString() + Environment.NewLine
+                        + "Category: " + ele.Category.Name + Environment.NewLine
+                        + "Instance: " + ele.Name + Environment.NewLine
+                        + "Symbol: " + eType.Name + Environment.NewLine
+                        + "Family: " + eType.FamilyName);
                 }
 
                 return Result.Succeeded;
